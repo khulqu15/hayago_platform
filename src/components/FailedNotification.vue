43b4c5299067f675448d7c44001b60440b14100c
Zienararya <90743006+Zienararya@users.noreply.github.com>
2024-01-23 10:10:47 +0700

    add notification component

---
+++ src/components/FailedNotification.vue
@@ -0,0 +1,9 @@
+<template>
+  <div role="alert" class="flex bg-error rounded-xl p-3 relative">
+    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="white" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+    <h1 class="text-white">Error! Login has been failed.</h1>
+    <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 text-white">✕</button>
+  </div>
+</template>
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,131 +1,133 @@
 <template>
-    <ion-page>
-        <ion-content :fullscreen="true">
-            <div class="relative min-h-screen overflow-hidden bg-base-100">
-                <div class="relative p-4">
-                    <div class="flex justify-between">
-                        <Icon role="button" @click="$router.replace('/')" icon="akar-icons:arrow-left" class="text-2xl text-base-content"/>
-                        <label for="language_modal" class="btn btn-sm font-maisonneue btn-ghost border border-base-content text-sm text-base-content rounded-xl px-4 flex items-center gap-x-3">
-                            <Icon icon="akar-icons:language" />
-                            <span>Bahasa Indonesia</span>
-                        </label>
-                        <input type="checkbox" id="language_modal" class="modal-toggle" />
-                        <div class="modal modal-bottom sm:modal-middle" role="dialog">
-                            <div class="modal-box text-base-content">
-                                <h3 class="font-bold text-lg mb-0 font-maisonneue">Pilih Bahasamu</h3>
-                                <p>Mau bahasa apa ?</p>
-                                <div class="my-5 space-y-2">
-                                    <div class="flex justify-between items-center border-1 border-b py-3 border-base-300">
-                                        <span class="font-bold">Bahasa Indonesia</span>
-                                        <input type="radio" name="radio-2" class="radio radio-primary" checked />
-                                    </div>
-                                    <div class="flex justify-between items-center border-1 border-b py-3 border-base-300">
-                                        <span class="font-bold">English</span>
-                                        <input type="radio" name="radio-2" class="radio radio-primary"/>
-                                    </div>
-                                    <div class="flex justify-between items-center border-1 border-b py-3 border-base-300">
-                                        <span class="font-bold">日本</span>
-                                        <input type="radio" name="radio-2" class="radio radio-primary"/>
-                                    </div>
-                                </div>
-                                <div class="modal-action">
-                                    <label for="language_modal" class="btn w-full bg-blue-600 text-white rounded-2xl">Simpan dan Lanjut</label>
-                                </div>
-                            </div>
-                            <label class="modal-backdrop" for="language_modal"></label>
-                        </div>
-                    </div>
-                    <div class="relative left-0 -bottom-2 mt-8 text-base-content bg-gradient-to-t from-base-100">
-                        <h1 class="font-maisonneue font-bold text-md">Selamat datang di Hayago!</h1>
-                        <p class="text-sm">Gunakan mission planner dalam beberapa langkah mudah</p>
-                        <form @submit.prevent="onSubmit()">
-                            <div class="form-control mt-4">
-                                <label for="email" class="text-sm mb-2">Email <span class="text-red-500">*</span></label>
-                                <div class="flex items-center gap-x-3">
-                                    <Icon icon="basil:envelope-solid" class="text-xl"/>
-                                    <input v-model="email" type="email" placeholder="contoh@domain.com" required class="input w-full">
-                                    <Icon v-if="email.length > 0" @click="email = ''" icon="uis:times-circle" class="text-2xl"/>
-                                </div>
-                            </div>
-                            <div v-if="next" class="form-control mt-2">
-                                <label for="email" class="text-sm mb-2">Password <span class="text-red-500">*</span></label>
-                                <div class="flex items-center gap-x-3">
-                                    <Icon @click="showPassword = !showPassword" icon="tabler:eye-filled" class="text-xl"/>
-                                    <input v-model="password" :type="showPassword ? 'text' : 'password'" placeholder="Secret password" required class="input w-full">
-                                    <Icon v-if="password.length > 0" @click="password = ''" icon="uis:times-circle" class="text-2xl"/>
-                                </div>
-                            </div>
-                            <div class="mt-2">
-                                <button type="submit" class="btn font-maisonneue bg-blue-600 text-white rounded-2xl w-full">Lanjut</button>
-                            </div>
-                        </form>
-                        <button @click="$router.replace('/reset')" class="btn rounded-xl mt-3 bg-base-100 border">Lupa atau ada kendala ?</button>
-                        <div class="my-4 flex items-center justify-center">
-                            <img src="/images/logo.png" class="w-10" alt="Hayago logo">
-                        </div>
-                    </div>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <div class="relative min-h-screen overflow-hidden bg-base-100">
+        <div class="relative p-4">
+          <div class="flex justify-between">
+            <Icon role="button" @click="$router.replace('/')" icon="akar-icons:arrow-left" class="text-2xl text-base-content" />
+            <label for="language_modal" class="btn btn-sm font-maisonneue btn-ghost border border-base-content text-sm text-base-content rounded-xl px-4 flex items-center gap-x-3">
+              <Icon icon="akar-icons:language" />
+              <span>Bahasa Indonesia</span>
+            </label>
+            <input type="checkbox" id="language_modal" class="modal-toggle" />
+            <div class="modal modal-bottom sm:modal-middle" role="dialog">
+              <div class="modal-box text-base-content">
+                <h3 class="font-bold text-lg mb-0 font-maisonneue">Pilih Bahasamu</h3>
+                <p>Mau bahasa apa ?</p>
+                <div class="my-5 space-y-2">
+                  <div class="flex justify-between items-center border-1 border-b py-3 border-base-300">
+                    <span class="font-bold">Bahasa Indonesia</span>
+                    <input type="radio" name="radio-2" class="radio radio-primary" checked />
+                  </div>
+                  <div class="flex justify-between items-center border-1 border-b py-3 border-base-300">
+                    <span class="font-bold">English</span>
+                    <input type="radio" name="radio-2" class="radio radio-primary" />
+                  </div>
+                  <div class="flex justify-between items-center border-1 border-b py-3 border-base-300">
+                    <span class="font-bold">日本</span>
+                    <input type="radio" name="radio-2" class="radio radio-primary" />
+                  </div>
                 </div>
+                <div class="modal-action">
+                  <label for="language_modal" class="btn w-full bg-blue-600 text-white rounded-2xl">Simpan dan Lanjut</label>
+                </div>
+              </div>
+              <label class="modal-backdrop" for="language_modal"></label>
             </div>
-        </ion-content>
-    </ion-page>
+          </div>
+          <div class="relative left-0 -bottom-2 mt-8 text-base-content bg-gradient-to-t from-base-100">
+            <h1 class="font-maisonneue font-bold text-md">Selamat datang di Hayago!</h1>
+            <p class="text-sm">Gunakan mission planner dalam beberapa langkah mudah</p>
+            <form @submit.prevent="onSubmit()">
+              <div class="form-control mt-4">
+                <label for="email" class="text-sm mb-2">Email <span class="text-red-500">*</span></label>
+                <div class="flex items-center gap-x-3">
+                  <Icon icon="basil:envelope-solid" class="text-xl" />
+                  <input v-model="email" type="email" placeholder="contoh@domain.com" required class="input w-full" />
+                  <Icon v-if="email.length > 0" @click="email = ''" icon="uis:times-circle" class="text-2xl" />
+                </div>
+              </div>
+              <div v-if="next" class="form-control mt-2">
+                <label for="email" class="text-sm mb-2">Password <span class="text-red-500">*</span></label>
+                <div class="flex items-center gap-x-3">
+                  <Icon @click="showPassword = !showPassword" icon="tabler:eye-filled" class="text-xl" />
+                  <input v-model="password" :type="showPassword ? 'text' : 'password'" placeholder="Secret password" required class="input w-full" />
+                  <Icon v-if="password.length > 0" @click="password = ''" icon="uis:times-circle" class="text-2xl" />
+                </div>
+              </div>
+              <div class="mt-2">
+                <button type="submit" class="btn font-maisonneue bg-blue-600 text-white rounded-2xl w-full">Lanjut</button>
+              </div>
+            </form>
+            <button @click="$router.replace('/reset')" class="btn rounded-xl mt-3 bg-base-100 border">Lupa atau ada kendala ?</button>
+            <div class="my-4 flex items-center justify-center">
+              <img src="/images/logo.png" class="w-10" alt="Hayago logo" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue'
-import { onMounted, ref, Ref } from 'vue'
-import { getAuth, fetchSignInMethodsForEmail, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification } from 'firebase/auth'
-import { getDatabase, ref as dbRef, set } from 'firebase/database'
-import { useRouter } from 'vue-router'
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from "@ionic/vue";
+import { onMounted, ref, Ref } from "vue";
+import { getAuth, fetchSignInMethodsForEmail, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
+import { getDatabase, ref as dbRef, set } from "firebase/database";
+import { useRouter } from "vue-router";
+import SuccessNotification from "@/components/SuccessNotification.vue";
+import FailedNotification from "@/components/FailedNotification.vue";
+import WarningNotification from "@/components/WarningNotification.vue";
 
-const auth = getAuth()
-const database = getDatabase()
-const router = useRouter()
-const email: Ref<string> = ref('khusnul.ninno15@gmail.com')
-const next: Ref<boolean> = ref(false)
-const password: Ref<string> = ref('12345678')
-const showPassword: Ref<boolean> = ref(false)
+const auth = getAuth();
+const database = getDatabase();
+const router = useRouter();
+const email: Ref<string> = ref("khusnul.ninno15@gmail.com");
+const next: Ref<boolean> = ref(false);
+const password: Ref<string> = ref("12345678");
+const showPassword: Ref<boolean> = ref(false);
 
 onMounted(() => {
-    const uid = sessionStorage.getItem('uid')
-    if(uid != null || uid != undefined) router.replace('/dashboard')
-})
+  const uid = sessionStorage.getItem("uid");
+  if (uid != null || uid != undefined) router.replace("/dashboard");
+});
 
 async function onSubmit() {
-    if(!next.value) next.value = true
-    else {
-        registerUser() 
-    } 
+  if (!next.value) next.value = true;
+  else {
+    registerUser();
+  }
 }
 
 async function loginUser() {
-    try {
-        console.log("Login")
-        const userCredential = await signInWithEmailAndPassword(auth, email.value, password.value)
-        const user = userCredential.user
-        sessionStorage.setItem('uid', user.uid)
-        await set(dbRef(database, 'users/'+user.uid+'/status'), 'active')
-        router.replace('/pin')
-    } catch(error) {
-        console.log(error)
-    }
+  try {
+    console.log("Login");
+    const userCredential = await signInWithEmailAndPassword(auth, email.value, password.value);
+    const user = userCredential.user;
+    sessionStorage.setItem("uid", user.uid);
+    await set(dbRef(database, "users/" + user.uid + "/status"), "active");
+    router.replace("/pin");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function registerUser() {
-    try {
-        console.log("Register")
-        const userCredential = await createUserWithEmailAndPassword(auth, email.value, password.value)
-        const user = userCredential.user
-        sessionStorage.setItem('uid', user.uid)
-        await sendEmailVerification(user)
-        await set(dbRef(database, 'users/'+user.uid), {
-            email: user.email,
-            uid: user.uid,
-            status: 'active',
-        })
-        router.replace('/register')
-        
-    } catch(error) {
-        loginUser()
-    }
+  try {
+    console.log("Register");
+    const userCredential = await createUserWithEmailAndPassword(auth, email.value, password.value);
+    const user = userCredential.user;
+    sessionStorage.setItem("uid", user.uid);
+    await sendEmailVerification(user);
+    await set(dbRef(database, "users/" + user.uid), {
+      email: user.email,
+      uid: user.uid,
+      status: "active",
+    });
+    router.replace("/register");
+  } catch (error) {
+    loginUser();
+  }
 }
 </script>
